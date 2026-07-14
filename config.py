45db30b0b9597895170f88cb2dd20cd436eefcde
si1entkerne1 <rishabhks651@gmail.com>
2026-07-14 20:56:52 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Core default definitions for easy modification
+DEFAULT_PORTS = [20, 21, 22, 23, 80, 110, 139, 443, 445, 3389] # Common ports list
+
+SCAN_PROTOCOLS = {
+    'SYN': 'TCP SYN',
+    'CONNECT': 'TCP Connect()',
+    'UDP': 'UDP Check',
+}
